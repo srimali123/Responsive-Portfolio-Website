852583f738d5775faa59a41bd54a48c8ae7594fa
srimali <you@example.com>
2021-05-25 03:51:59 +0530

    code update

--- script.js
+++ script.js
@@ -7,4 +7,9 @@ $(window).scroll(function(){
 
     }
 })
+// toggle menu/navbar script
+   $('.menu-btn').click(function(){
+       $('.navbar .menu').toggleClass("active");
+       $('.menu-btn i').toggleClass("active");
+   })
 });
