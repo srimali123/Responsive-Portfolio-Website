365abfbfd2ee66f0779fee243f559276a49b2449
srimali <srimaliudayangani850@gmail.com>
2021-06-12 00:17:25 +0530

    update js and css and html code

--- script.js
+++ script.js
@@ -65,4 +65,34 @@ $('.scroll-up-btn').click(function(){
           }
           
     });
+    
+
+    $('.carousel2').owlCarousel({
+        margin:0,
+        loop:true,
+        autoplayTimeOut:2000,
+        autoplayHoverPause:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+              items:2,
+              nav:false
+          },
+          1000:{
+              items:3,
+              nav:false
+          }
+        }
+        
+  });
+
+
+
+
+
+
+
 });
